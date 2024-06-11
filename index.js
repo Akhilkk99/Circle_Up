8bb8430eecb26fefab27b8b06a01c4ecdcebfc24
Ak hi <akhilkk4219@gmail.com>
2024-06-11 16:06:33 +0530

    test2

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.use("/api/v1", post);
 app.use("/api/v1", user);
 
 // serving static files for frontend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 app.listen(PORT, () => {
     console.log(`server started on port: ${PORT}`);
